bde221c01cd56d9739b29adfbd618ed661083397
Mithun V <bipinuav@gmail.com>
2025-06-14 19:51:43 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,26 +8,34 @@
 #include <string>
 
 int main() {
-    // Input string
+    // Input text to be encoded
     std::string inputText = "huffman encoding test";
     std::vector<unsigned char> inputData(inputText.begin(), inputText.end());
 
-    // Encode
+    // Build the Huffman encoder and generate code map
     Encoder encoder;
     encoder.build(inputData);
-    printCodeMap(encoder.getCodeMap());
+
+    // Print the symbol-to-code mapping
+    printCodeMap(encoder.codeMap());
+
+    // Encode the input data into a sequence of bits
     std::vector<bool> encodedBits = encoder.encode(inputData);
 
     std::cout << "Original text: " << inputText << "\n";
+
+    // Output the encoded bit stream
     std::cout << "Encoded bits: ";
     for (bool bit : encodedBits) {
         std::cout << bit;
     }
     std::cout << "\n";
 
-    // Decode
+    // Set up the decoder using the same Huffman tree
     Decoder decoder;
     decoder.setTree(encoder.getRoot());
+
+    // Decode the bit sequence back into the original bytes
     std::vector<unsigned char> decodedData = decoder.decode(encodedBits);
     std::string decodedText(decodedData.begin(), decodedData.end());
 
