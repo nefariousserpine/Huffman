9e56019e528e61954fe40b178dc76a9973b25f04
Mithun V <bipinuav@gmail.com>
2025-07-23 15:42:48 +0530

    Update PrintMap.hpp

--- src/PrintMap.hpp
+++ src/PrintMap.hpp
@@ -2,5 +2,4 @@
 #include <unordered_map>
 #include <vector>
 
-// Prints the symbol-to-code mapping (Huffman codes) to standard output.
 void printCodeMap(const std::unordered_map<unsigned char, std::vector<bool>>& codeMap);
