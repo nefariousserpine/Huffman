2f904d424ccaa625861fbffb063bcef9230aea19
Mithun V <bipinuav@gmail.com>
2025-07-23 15:43:33 +0530

    Update FileEncoder.hpp

--- src/FileEncoder.hpp
+++ src/FileEncoder.hpp
@@ -6,14 +6,11 @@
 
 class FileEncoder {
 public:
-    // encode contents of input file, write the Huffman tree and encoded data to output file
     void encodeFile(const std::string& inputFile, const std::string& outputFile);
 
 private:
-    // write bits as packed bytes
     void writeBits(std::ostream& out, const std::vector<bool>& bits) const;
 
-    // serialize the Huffman tree into a binary stream
     void serializeTree(std::ostream& out, const TreeNode::Ptr& node) const;
 };
 
