90df2462c4811eeafceea17440ba52c0876d9f2f
Mithun V <bipinuav@gmail.com>
2025-07-23 15:46:32 +0530

    Update Decoder.hpp

--- src/Decoder.hpp
+++ src/Decoder.hpp
@@ -4,21 +4,17 @@
 #include "TreeNode.hpp"
 #include <vector>
 
-// Huffman Decoder.
-// Decodes a bit sequence into original symbols using a given Huffman tree.
 class Decoder {
     public:
         Decoder() = default;
         ~Decoder() = default;
 
-        // Sets the Huffman tree to be used for decoding.
         void setTree(const TreeNode::Ptr& treeRoot);
 
-        // Decodes a bit sequence into a byte sequence using the Huffman tree.
         std::vector<unsigned char> decode(const std::vector<bool>& bits) const;
 
     private:
-        TreeNode::Ptr root_; // Root of the Huffman tree.
+        TreeNode::Ptr root_; 
 };
 
 #endif 
